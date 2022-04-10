e549de4525dd577d993eabbba23f4f770687e3fe
EC2 Default User <ec2-user@ip-172-31-82-51.ec2.internal>
2022-04-10 13:24:54 +0000

    ルーティング省略化

--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,32 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'platform/index'
-    get 'platform/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'games/index'
-    get 'games/new'
-    get 'games/show'
-    get 'games/edit'
-  end
-  namespace :customer do
-    get 'bookmark/index'
-  end
-  namespace :customer do
-    get 'customers/show'
-    get 'customers/quit'
-    get 'customers/edit'
-  end
-  namespace :customer do
-    get 'comments/new'
-    get 'comments/edit'
-  end
-  namespace :customer do
-    get 'reviews/new'
-    get 'reviews/show'
-    get 'reviews/edit'
-  end
-  namespace :customer do
-    get 'homes/top'
-  end
-  namespace :customer do
-    get 'games/index'
-    get 'games/show'
-    get 'games/new'
-  end
+
   #顧客用
-  namespace :customer do
-    
-  end
-  
-  devise_for :customers,skip: [:passwords], controllers: {
+   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "customer/registrations",
     sessions: "customer/sessions"
   }
+
+
+  namespace :customer do
+    resources :bookmarks, only: [:index]
+    resources :customers, only: [:show, :quit, :edit]
+    resources :comments, only: [:new, :edit]
+    resources :reviews, only: [:new, :show, :edit]
+    resources :games, only: [:index, :show, :new]
+  end
+  root to: "customer/homes#top"
+
 
   #管理者用
   devise_for :admins,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
-    
+    resources :customers, only: [:index, :show, :edit]
+    resources :platforms, only: [:index, :edit]
+    resources :genres, only: [:index, :edit]
+    resources :games, only: [:index, :new, :show, :edit]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
