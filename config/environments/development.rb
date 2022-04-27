cbbffdb085710a5e24f3e426ed7450e4d9423b64
Yuna0203 <citron5553@gmail.com>
2022-04-27 20:25:21 +0000

    全移動

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.active_job.queue_adapter = :inline
-  config.hosts << "aac06ed516a14eea9316ae4f0c0e6fd1.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "cac25a65835140bab1dd90f4c0b84971.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
